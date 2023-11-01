5137f0ccfce16955fb5319c5d7a2c3941135d173
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-11-01 20:07:49 +0200

    change

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,8 +1,8 @@
 import { postgres } from "../deps.js";
 
 let sql;
-if (Deno.env.get("FLYWAY_URL")) {
-  sql = postgres(Deno.env.get("FLYWAY_URL"));
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
   sql = postgres({});
 }
