eb405b66658d528c54bf98a98bfa9a4fd16078f5
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-11-01 19:28:33 +0200

    change database

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,11 +1,10 @@
 import { postgres } from "../deps.js";
 
 let sql;
-if (Deno.env.get("database-p1-4f39a9d5-9b37-4b13-b7d4-5693677e9d48L")) {
-  sql = postgres(Deno.env.get("database-p1-4f39a9d5-9b37-4b13-b7d4-5693677e9d48"));
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
   sql = postgres({});
 }
-
 
 export { sql };
