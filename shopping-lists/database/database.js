5b8dda00bb23936db7b96cab9a161bb69c1f075d
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-11-02 00:28:36 +0200

    change

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,10 +1,12 @@
 import { postgres } from "../deps.js";
 
-let sql;
-if (Deno.env.get("DATABASE_URL")) {
-  sql = postgres(Deno.env.get("DATABASE_URL"));
-} else {
-  sql = postgres({});
-}
+const sql = postgres({
+  host: Deno.env.get("PGHOST"), // Use the environment variable PGHOST
+  database: Deno.env.get("PGDATABASE"), // Use the environment variable PGDATABASE
+  username: Deno.env.get("PGUSER"), // Use the environment variable PGUSER
+  password: Deno.env.get("PGPASSWORD"), // Use the environment variable PGPASSWORD
+  port: 5432,
+  max: 2, // use at most 2 concurrent connections
+});
 
 export { sql };
