adec09b8620fc37e4ed6e804d109f173acf0159c
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-11-02 11:06:04 +0200

    change database

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,11 +1,10 @@
 import { postgres } from "../deps.js";
 
-const sql = postgres({
-  host: Deno.env.get("PGHOST"), 
-  database: Deno.env.get("PGDATABASE"), 
-  username: Deno.env.get("PGUSER"), 
-  password: Deno.env.get("PGPASSWORD"), 
-  port: Deno.env.get("PGPORT"),
-});
+let sql;
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
+} else {
+  sql = postgres({});
+}
 
 export { sql };
